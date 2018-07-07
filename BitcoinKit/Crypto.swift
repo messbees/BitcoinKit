e7abc4fe203c09efc8f9db1d6574189ec7f9dc5c
Anton Betsun <anton.betsun@sharekey.com>
2018-07-07 16:08:16 +0300

    add signCompact function

--- BitcoinKit/Crypto.swift
+++ BitcoinKit/Crypto.swift
@@ -53,6 +53,27 @@ public struct Crypto {
 
         return der
     }
+    
+    public static func signCompact(_ data: Data, privateKey: PrivateKey) throws -> Data {
+        let ctx = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN))!
+        defer { secp256k1_context_destroy(ctx) }
+        let signature = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+        defer { signature.deallocate(capacity: 1) }
+        let status = data.withUnsafeBytes { (ptr: UnsafePointer<UInt8>) in
+            privateKey.raw.withUnsafeBytes {
+                secp256k1_ecdsa_sign(ctx, signature, ptr, $0, nil, nil)
+            }
+        }
+        guard status == 1 else { throw CryptoError.signFailed }
+        let normalizedsig = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+        defer { normalizedsig.deallocate(capacity: 1) }
+        secp256k1_ecdsa_signature_normalize(ctx, normalizedsig, signature)
+        let length: size_t = 64
+        var compact = Data(count: length)
+        guard compact.withUnsafeMutableBytes( { return secp256k1_ecdsa_signature_serialize_compact(ctx, $0, normalizedsig) }) == 1 else { throw CryptoError.noEnoughSpace }
+        compact.count = length
+        return compact
+    }
 }
 
 public enum CryptoError : Error {
